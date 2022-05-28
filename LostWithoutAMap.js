69e8a87e5dc8dd26a353f484a384b399eee2cdd8
Diego Herrera <dagomessis@gmail.com>
2022-05-28 10:26:14 -0400

    19

---
+++ LostWithoutAMap.js
@@ -0,0 +1,12 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+//Answer:
+
+function maps(arr) {
+  const newArr = arr.map((x) => x * 2);
+  return newArr;
+}
