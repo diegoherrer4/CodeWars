7632a496757a0f24d57080f7f9a7434f41228e64
Diego Herrera <dagomessis@gmail.com>
2022-06-02 10:27:52 -0400

    24

---
+++ ReversedString2.js
@@ -0,0 +1,9 @@
+//24
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+//Answer:
+
+const solution = (str) => str.split("").reverse().join("");
