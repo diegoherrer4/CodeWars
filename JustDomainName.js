3bc781edb036b08e97815c0d050260951f8f24a1
Diego Herrera <dagomessis@gmail.com>
2022-10-13 18:20:57 -0400

    104

---
+++ JustDomainName.js
@@ -0,0 +1,21 @@
+//104
+
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// domainName("http://github.com/carbonfive/raygun") == "github" 
+// domainName("http://www.zombie-bites.com") == "zombie-bites"
+// domainName("https://www.cnet.com") == "cnet"
+
+
+//url trad no funny biz
+//str
+
+function parseDomainName(url){
+
+//replace the stuff upfront
+return url.replace('https://','').replace('http://','').replace('www','').split('.')[0]
+//split grabbing the first value
+
+} 
+
+console.log(parseDomainName("http://github.com/carbonfive/raygun"), 'github')
