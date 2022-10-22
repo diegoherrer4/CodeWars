d83506659623a6d2e22ad061b24335e0971a5e95
Diego Herrera <dagomessis@gmail.com>
2022-10-22 14:10:31 -0400

    108

---
+++ ReverseString2.js
@@ -0,0 +1,14 @@
+// 108
+
+//Given a string, return the string with the characters in reversed order
+//String
+//String
+const reversedString = (str) => {
+    //str to array using split -> reverse array -> join array
+   return str.split('').reverse().join('')
+}
+
+
+
+console.log(reverseString('Hello'), 'elloH') // olleH
+console.log(reverseString('Reverse String'), 'gnirtS esreveR') // gnirtS esreveR
