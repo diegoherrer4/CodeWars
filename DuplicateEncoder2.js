6722f4331af00afd423256ea8895251a3c15b21a
Diego Herrera <dagomessis@gmail.com>
2022-10-28 19:01:39 -0400

    111

--- DuplicateEncoder2.js
+++ DuplicateEncoder2.js
@@ -12,7 +12,7 @@
 // Assertion messages may be unclear about what they display in some languages. 
 // If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
 
-function duplicateEncode(word){
+const duplicateEncode = (word) => {
     let newWord = word.toLowerCase()
     let result = ""
     for(i of newWord){
